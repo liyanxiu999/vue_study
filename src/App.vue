336b9278c6bb1d7804c7a48a4fe9a7b5d64dad58
liyanxiu999 <805024578@qq.com>
2020-12-12 16:16:15 +0800

    用element-ui完成todolist DEMO

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="less">
+* {
+  margin: 0;
+  padding: 0;
+}
+html,body {
+  width: 100%;
+  height: 100%;
+}
+#app {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
